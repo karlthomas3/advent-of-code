860a13c62efb19d997c029948a703074989531d6
karl Thomas <karlthomas3@icloud.com>
2024-12-03 11:59:43 +0000

    2024/elixir/day03 done. I hate regex

---
+++ 2024/elixir/aoc/lib/day03.ex
@@ -0,0 +1,34 @@
+defmodule Aoc.Day03 do
+  def part1(input) do
+    regex = ~r/mul\((\d{1,3}),(\d{1,3})\)/
+
+    Regex.scan(regex, input)
+    |> Enum.map(fn [_full_match, x, y] -> String.to_integer(x) * String.to_integer(y) end)
+    |> Enum.sum()
+    |> IO.puts()
+  end
+
+  def part2(input) do
+    regex = ~r/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/
+    matches = Regex.scan(regex, input)
+
+    {_, result} =
+      Enum.reduce(matches, {true, []}, fn
+        ["do()"], {_, acc} ->
+          {true, acc}
+
+        ["don't()"], {_, acc} ->
+          {false, acc}
+
+        ["mul(" <> _ = _full_match, x, y], {true, acc} ->
+          {true, [String.to_integer(x) * String.to_integer(y) | acc]}
+
+        ["mul(" <> _ = _full_match, _x, _y], {false, acc} ->
+          {false, acc}
+      end)
+
+    result
+    |> Enum.sum()
+    |> IO.puts()
+  end
+end
