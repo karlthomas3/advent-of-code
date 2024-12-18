65d51651ecaa8adada4e5cefcb8e6782d24010d5
karl Thomas <karlthomas3@icloud.com>
2024-12-18 16:30:55 +0000

    2024/elixir/day10 part1 done

---
+++ 2024/elixir/aoc/lib/day10.ex
@@ -0,0 +1,71 @@
+defmodule Aoc.Day10 do
+  import Aoc.Utils
+
+  @spec part1(String.t()) :: :ok
+  def part1(input) do
+    input
+    |> split_into_grid(true)
+    |> count_all_paths()
+    |> IO.puts()
+  end
+
+  @spec in_bounds?([[integer()]], {integer(), integer()}) :: boolean()
+  defp in_bounds?(grid, {x, y}) do
+    x >= 0 and y >= 0 and x < length(grid) and y < length(Enum.at(grid, 0))
+  end
+
+  @spec get_directions([[String.t()]], {integer(), integer()}) :: [{integer(), integer()}]
+  defp get_directions(grid, {x, y}) do
+    directions = [
+      # up
+      {-1, 0},
+      # down
+      {1, 0},
+      # right
+      {0, 1},
+      # left
+      {0, -1}
+    ]
+
+    directions
+    |> Enum.map(fn {dx, dy} ->
+      {x + dx, y + dy}
+    end)
+    |> Enum.filter(&in_bounds?(grid, &1))
+  end
+
+  @spec check_directions([[integer()]], {integer(), integer()}) :: [{integer(), integer()}]
+  defp check_directions(grid, {x, y}) do
+    central_value = value_from_grid_pos(grid, {x, y})
+    target_value = central_value + 1
+
+    get_directions(grid, {x, y})
+    |> Enum.filter(fn {adj_x, adj_y} ->
+      value_from_grid_pos(grid, {adj_x, adj_y}) == target_value
+    end)
+  end
+
+  @spec count_paths_from_pos([[integer()]], {integer(), integer()}, MapSet.t()) :: MapSet.t()
+  defp count_paths_from_pos(grid, {x, y}, visited) do
+    if value_from_grid_pos(grid, {x, y}) == 9 do
+      MapSet.put(visited, {x, y})
+    else
+      check_directions(grid, {x, y})
+      |> Enum.reduce(visited, fn {adj_x, adj_y}, acc ->
+        count_paths_from_pos(grid, {adj_x, adj_y}, acc)
+      end)
+    end
+  end
+
+  @spec count_all_paths([[integer()]]) :: integer()
+  defp count_all_paths(grid) do
+    for x <- 0..(length(grid) - 1),
+        y <- 0..(length(Enum.at(grid, 0)) - 1),
+        value_from_grid_pos(grid, {x, y}) == 0,
+        reduce: 0 do
+      acc ->
+        unique_nines = count_paths_from_pos(grid, {x, y}, MapSet.new())
+        acc + MapSet.size(unique_nines)
+    end
+  end
+end
--- 2024/elixir/aoc/lib/utils.ex
+++ 2024/elixir/aoc/lib/utils.ex
@@ -1,12 +1,29 @@
 defmodule Aoc.Utils do
+  @spec split_by_newline(String.t()) :: [String.t()]
   def split_by_newline(input) do
     input
     |> String.split("\n", trim: true)
   end
 
-  def split_into_grid(input) do
-    input
-    |> split_by_newline()
-    |> Enum.map(&String.split(&1, "", trim: true))
+  @spec split_into_grid(String.t(), boolean()) :: [[String.t() | integer()]]
+  def split_into_grid(input, numbers \\ false) do
+    grid =
+      input
+      |> String.trim()
+      |> split_by_newline()
+      |> Enum.map(&String.split(&1, "", trim: true))
+
+    if numbers == true do
+      grid
+      |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+    else
+      grid
+    end
+  end
+
+  @spec value_from_grid_pos([[integer() | String.t()]], {integer(), integer()}) ::
+          String.t() | integer()
+  def value_from_grid_pos(grid, {x, y}) do
+    Enum.at(Enum.at(grid, x), y)
   end
 end
