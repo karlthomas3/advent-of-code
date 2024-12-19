301cf57fe40fb4d986100c8cc00e01b32194d70c
karl Thomas <karlthomas3@icloud.com>
2024-12-19 11:24:59 +0000

    2024/elixir/day10 part2 done

--- 2024/elixir/aoc/lib/day10.ex
+++ 2024/elixir/aoc/lib/day10.ex
@@ -9,6 +9,14 @@ defmodule Aoc.Day10 do
     |> IO.puts()
   end
 
+  @spec part2(Sting.t()) :: :ok
+  def part2(input) do
+    input
+    |> split_into_grid(true)
+    |> count_all_distinct_paths()
+    |> IO.puts()
+  end
+
   @spec in_bounds?([[integer()]], {integer(), integer()}) :: boolean()
   defp in_bounds?(grid, {x, y}) do
     x >= 0 and y >= 0 and x < length(grid) and y < length(Enum.at(grid, 0))
@@ -21,10 +29,10 @@ defmodule Aoc.Day10 do
       {-1, 0},
       # down
       {1, 0},
-      # right
-      {0, 1},
       # left
-      {0, -1}
+      {0, -1},
+      # right
+      {0, 1}
     ]
 
     directions
@@ -66,6 +74,30 @@ defmodule Aoc.Day10 do
       acc ->
         unique_nines = count_paths_from_pos(grid, {x, y}, MapSet.new())
         acc + MapSet.size(unique_nines)
+    end
+  end
+
+  @spec count_distinct_paths_from_pos([[integer()]], {integer(), integer()}) :: integer()
+  defp count_distinct_paths_from_pos(grid, {x, y}) do
+    cur_val = value_from_grid_pos(grid, {x, y})
+
+    if cur_val == 9 do
+      1
+    else
+      check_directions(grid, {x, y})
+      |> Enum.reduce(0, fn {adj_x, adj_y}, acc ->
+        acc + count_distinct_paths_from_pos(grid, {adj_x, adj_y})
+      end)
+    end
+  end
+
+  @spec count_all_distinct_paths([[integer()]]) :: integer()
+  defp count_all_distinct_paths(grid) do
+    for x <- 0..(length(grid) - 1),
+        y <- 0..(length(Enum.at(grid, 0)) - 1),
+        value_from_grid_pos(grid, {x, y}) == 0,
+        reduce: 0 do
+      acc -> acc + count_distinct_paths_from_pos(grid, {x, y})
     end
   end
 end
